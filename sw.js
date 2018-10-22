3ca9d34cf2781323a33c74ef8bf0ba0b2126e6ea
Oliver Martinez <oliverio.oo@gmail.com>
2018-10-22 18:55:32 -0500

    Se modifico la ruta del SW

--- sw.js
+++ sw.js
@@ -23,8 +23,8 @@ const APP_SHELL_INM = [
     'https://fonts.googleapis.com/css?family=Quicksand:300,400',
     'https://fonts.googleapis.com/css?family=Lato:400,300',
     'https://use.fontawesome.com/releases/v5.3.1/css/all.css',
-    '/css/animate.css',
-    '/js/libs/jquery.js'
+    'css/animate.css',
+    'js/libs/jquery.js'
 ];
 
 self.addEventListener('install', e => {

ace4d76b0f1a6ba896b724a89e66c19bd0e66552
Oliver Martinez <oliverio.oo@gmail.com>
2018-10-22 14:07:45 -0500

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -6,17 +6,17 @@ const DYNAMIC_CACHE = 'dynamic-1';
 const INM_CACHE     = 'inm-1';
 
 const APP_SHELL = [
-    '/',
-    '/index.html',
-    '/css/style.css',
-    '/img/favicon.ico',
-    '/img/avatars/hulk.jpg',
-    '/img/avatars/ironman.jpg',
-    '/img/avatars/spiderman.jpg',
-    '/img/avatars/thor.jpg',
-    '/img/avatars/wolverine.jpg',
-    '/js/app.js',
-    '/js/sw-utils.js'
+    // '/',
+    'index.html',
+    'css/style.css',
+    'img/favicon.ico',
+    'img/avatars/hulk.jpg',
+    'img/avatars/ironman.jpg',
+    'img/avatars/spiderman.jpg',
+    'img/avatars/thor.jpg',
+    'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
 ];
 
 const APP_SHELL_INM = [
